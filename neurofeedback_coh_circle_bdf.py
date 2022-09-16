b802f04fb724be96cccd1946ba79a1d400ac08c9
dmitry-sukhoruchkin <dmitry.sukhoruchkin@gmail.com>
2022-09-16 23:02:42 +0300

    Add files via upload

--- neurofeedback_coh_circle_bdf.py
+++ neurofeedback_coh_circle_bdf.py
@@ -35,7 +35,7 @@ flags.DEFINE_string('input_name', 'neurofeedback', 'input')
 flags.DEFINE_string('serial_port', '/dev/ttyACM0', 'serial_port')
 #flags.DEFINE_list('prefix', None, 'prefix')
 flags.DEFINE_string('output_path', '', 'output_path')
-flags.DEFINE_string('output', None, 'output, if None, used: output_path/input_name+"_%Y-%m-%d_%H:%M:%S.bdf"')
+flags.DEFINE_string('output', None, 'output, if None, used: output_path+input_name+"-%Y.%m.%d-%H.%M.%S.bdf"')
 flags.DEFINE_list('ch_names', ['FP1','AF3','F7','F3','FC1','FC5','T7','C3','CP1','CP5','P7','P3','Pz','PO3','O1','Oz','O2','PO4','P4','P8','CP6','CP2','C4','T8','FC6','FC2','F4','F8','AF4','FP2','Fz','Cz'], 'ch_names')
 flags.DEFINE_list('ch_names_pick', ['FP1','AF3','F7','F3','FC5','T7','C3','CP5','P7','P3','PO3','O1','Oz','CP1','FC1','Fz','Cz','FC2','CP2','Pz','O2','PO4','P4','P8','CP6','C4','T8','FC6','F4','F8','AF4','FP2']
 , 'ch_names')
@@ -221,14 +221,15 @@ if True:
 
   now = datetime.now()
 
-  dt_string = now.strftime("%Y-%m-%d_%H:%M:%S")
+  dt_string = now.strftime("%Y.%m.%d-%H.%M.%S")
+
+  output_path=FLAGS.output_path
   
   if FLAGS.output==None:
-    dst=input_name+"_"+dt_string+".bdf"
+    dst=output_path+input_name+"-"+dt_string+".bdf"
   else:
     dst=FLAGS.output
   
-  output_path=FLAGS.output_path
   
   pmax=300000
   dmax = 8388607
